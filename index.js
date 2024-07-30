ec97df8bf614968ba12d10fd58a8e85de69d2009
Alan Jose Santo <realityaudiostudio@gmail.com>
2024-07-30 15:34:30 +0530

    doo

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
+const port = 4550;
+
+app.get('/',(req,res)=>res.send('Now I am at port '+port));
+app.listen(port,console.log('Server runs at : '+port));
