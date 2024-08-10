e6e33fe2befa7a095fc136464633b76f87f0614c
Alan Jose Santo <realityaudiostudio@gmail.com>
2024-08-10 11:40:54 +0530

    Backend upto Products

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
 const port = 4550;
 
-app.get('/',(req,res)=>res.send('Now I am at port '+port));
-app.listen(port,console.log('Server runs at : '+port));
+let db;
+
+async function mongoConnect() {
+    let client = new MongoClient('mongodb+srv://realityaudiostudio:njan4abhi6siva@alanjs.ddjefp8.mongodb.net/?retryWrites=true&w=majority&appName=alanjs');
+    await client.connect();
+    db = client.db('muzify');
+ }
+
+ app.use(cors());
+app.use(express.json());
+
+app.get('/users',async function(req,res){
+  let output = await db.collection('users').find({}).toArray();
+  res.json(output);
+})
+
+app.post('/users',async function (req,res) {
+  let output = await db.collection('users').insertOne(req.body);
+  console.log(req.body);
+  res.json(output);
+})
+
+app.post('/login', async function(req, res) {
+  console.log('Login attempt:', req.body);
+  const { username, password } = req.body;
+  try {
+      let usr = await db.collection('users').findOne({ username, password });
+      if (usr) {
+          console.log('Login successful:', usr);
+          res.json({ success: true, usr });
+      } else {
+          console.log('Invalid USER or PASS');
+          res.json({ success: false, message: 'Invalid USER or PASS' });
+      }
+  } catch (error) {
+      console.error('Error during login:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/products',async function (req,res) {
+  let products = await db.collection('products').find({}).toArray();
+  res.json(products);
+  
+})
+
+
+
+app.get('/',(req,res)=>res.send('Nochu is live at port '+port));
+app.listen(port,function(){
+  console.log('Server runs at : '+port);
+  mongoConnect();
+});
+
+//
